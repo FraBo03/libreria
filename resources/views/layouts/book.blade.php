e4c613c0fb009f823e29c7848de1b4f9dee3f237
FraBo03 <bonavita.fra@gmail.com>
2024-11-07 12:03:05 +0100

    cambiato il titolo del blade books

--- resources/views/layouts/book.blade.php
+++ resources/views/layouts/book.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>ciao</title>
+    <title>Books</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/book.css') }}">
     <link rel="stylesheet" href="{{ asset('css/green_button.css') }}">
